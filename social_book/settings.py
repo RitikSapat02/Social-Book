5f63ba1605322f7831f1385c0c83d6649428e0ba
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2023-05-14 10:42:13 +0530

    Update settings.py

--- social_book/settings.py
+++ social_book/settings.py
@@ -114,7 +114,8 @@ STATIC_URL = 'static/'
 
 # STATIC_ROOT = BASE_DIR / 'assets'
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
